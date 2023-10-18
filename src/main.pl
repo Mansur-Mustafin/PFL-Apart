3910aed2b360e8771ca9e06125d546e5898c2ae6
Daniel Ferreira <unidsferreira2003@gmail.com>
2023-10-18 14:55:38 +0100

    Added main skeleton of game_loop

--- src/main.pl
+++ src/main.pl
@@ -1,12 +1,20 @@
 :- consult('view.pl').
 :- consult('state.pl').
 
-play :-
-	display_titel,
-	createBoard(Board),
-	game_state(Player, Board, Visited),
-	display_game([Player, Board, Visited]),
+valid_moves(Player-Board-Visited, Player, ValidMoves) :-
+    write('Todo'), nl.
 
+move(Player-Board-Visited, CurrPosCol-CurrPosRow-NewPosCol-NewPosRow, NewPlayer-NewBoard-NewVisited) :-
+    write('Todo'), nl.
+
+game_over(Player-Board-Visited, Winner) :-
+    write('Todo'), nl.
+
+game_loop(Player, Board, Visited) :-
+    display_game([Player, Board, Visited]),
+    valid_moves(Player-Board-Visited, Player, ValidMoves),
+
+    % Change this block of code to get a move ------------------------------------------------------
     write('Enter origin coordinates (Col-Row): '), nl,
     read(OriginCol-OriginRow),
     get_index(OriginCol, OrigColIndex), 
@@ -20,6 +28,17 @@ play :-
     DestRowIndex is DestRow - 1,
     % Call step predicate with user input
     step(Board, OrigColIndex-OriginRowIndex, DestColIndex-DestRowIndex, NewBoard),
-    
-    % Display the new board state
-    display_game([Player, NewBoard, Visited]).
+
+    % ------------------------------------------------------------------------------------------
+
+    move(Player-Board-Visited, CurrPosCol-CurrPosRow-NewPosCol-NewPosRow, NewPlayer-NewBoard-NewVisited),
+    % change player if visited is empty?
+    game_over(Player-Board-Visited, Winner),
+    game_loop(NewPlayer, NewBoard, NewVisited).
+
+
+play :-
+	display_titel,
+	createBoard(Board),
+	game_state(Player, Board, Visited),
+    game_loop(Player, Board, Visited).
