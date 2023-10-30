f8399045865b12dbf1b98fb869566bed7ed772a6
Mansur <mustafin.mansur02@gmail.com>
2023-10-30 11:37:40 +0000

    deleted unused function

--- src/main.pl
+++ src/main.pl
@@ -64,14 +64,7 @@ game_loop(Player-NextPlayer, Board, []) :-
     read_pos(OrigColIndex-OriginRowIndex),
 
     valid_piece_choice(Player-NextPlayer, Board, OrigColIndex-OriginRowIndex). % TODO: check if user selected the right piece.
-
-
-% case se burro escolheu peca nao dela ou nem peca
-game_loop(Player-NextPlayer, Board, []) :-
-    is_human(Player),
-    write('Invalid piece chosen. Please choose again.'), nl,
-    game_loop(Player-NextPlayer, Board, []).
-
+    
 
 game_loop(Player-NextPlayer, Board, [CurrPosCol-CurrPosRow|T]) :-
     is_human(Player),
